08fddbdc0ac70a549bac82131771218107186def
Khaled Hamed <kholioeg@gmail.com>
2016-04-15 04:23:25 +0300

    add discription

--- __openerp__.py
+++ __openerp__.py
@@ -3,10 +3,21 @@
     'name': "Account Discount",
 
     'summary': """
-        Apply Discount model to taxes""",
+        Use Tax model for discounts as well""",
 
     'description': """
-        The purpose is to apply discount record for the same tax model
+        
+        Odoo OpenERP Account Discount from Tax
+    
+This module adds new concept to use tax model as discount model and print both taxes and discounts separetly.
+
+The steps to perform are very easy:
+
+    First you define new tax with negative amount (e.g Name: Discount 10%, Amount: -0.10).
+    Enable Is Discount Checkbox.
+    Then add this dicount from the Taxes/Discounts column per invoice line.
+
+This way, you can separate and analyze discounts using different account/analytic account as well.
     """,
 
     'author': "Khaled Hamed",
